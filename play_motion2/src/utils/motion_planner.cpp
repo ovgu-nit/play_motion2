354ece1f3fe2042a0cf2877ff7a1a77d685f5884
Domas Strazdas <dominykas.strazdas@ovgu.de>
2026-07-08 09:24:45 +0200

    fix: Comment out warning for disabled motion planning capability

--- play_motion2/src/utils/motion_planner.cpp
+++ play_motion2/src/utils/motion_planner.cpp
@@ -137,10 +137,10 @@ void MotionPlanner::check_parameters()
   }
 
   if (planning_disabled_) {
-    RCLCPP_WARN(
-      node_->get_logger(),
-      "Motion planning capability DISABLED. Goals requesting planning (the default) "
-      "will be rejected. To disable planning in goal requests set 'skip_planning: true'");
+    // RCLCPP_WARN(
+    //   node_->get_logger(),
+    //   "Motion planning capability DISABLED. Goals requesting planning (the default) "
+    //   "will be rejected. To disable planning in goal requests set 'skip_planning: true'");
 
     return;  // Skip initialization of planning-related members when all planning is disabled
   }
